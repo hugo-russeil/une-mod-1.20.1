f45a597a62b3cd8c6a06888440583bad26adfc62
hrusseil <hugo.russeil@etudiant.univ-lr.fr>
2024-05-02 19:22:06 +0200

    Initial commit

---
+++ src/main/java/net/une/mod/UNEModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.une.mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UNEModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
