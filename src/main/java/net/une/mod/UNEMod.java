c157c4a50fc8794e64944937a9c134ee8db4fd46
hrusseil <hugo.russeil@etudiant.univ-lr.fr>
2024-05-02 20:31:03 +0200

    Edited template

--- src/main/java/net/une/mod/UNEMod.java
+++ src/main/java/net/une/mod/UNEMod.java
@@ -6,17 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UNEMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MODID = "une-mod";
     public static final Logger LOGGER = LoggerFactory.getLogger("une-mod");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/net/une/mod/UNEModClient.java
@@ -0,0 +1,9 @@
+package net.une.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+public class UNEModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
